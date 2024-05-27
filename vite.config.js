cdfc1653543c72f16a0633a0e5d266e19d61d4a4
adeel <adeelcomsats070@gmail.com>
2024-05-28 00:17:46 +0500

    cors error api url change

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // host: "192.168.100.5",
-    // port: 3000,
-    proxy: {
-      "/customer/generateotp": "https://78b9-59-103-109-25.ngrok-free.app",
-      "/customer/corptaglist": "https://78b9-59-103-109-25.ngrok-free.app",
-      "/customer/auth/register": "https://78b9-59-103-109-25.ngrok-free.app",
-      "/customer/auth/login": "https://78b9-59-103-109-25.ngrok-free.app",
-      "/customer/updatecustomer": "https://78b9-59-103-109-25.ngrok-free.app",
+  // server: {
+  //   // host: "192.168.100.5",
+  //   // port: 3000,
+  //   proxy: {
+  //     "/customer/generateotp": "https://78b9-59-103-109-25.ngrok-free.app",
+  //     "/customer/corptaglist": "https://78b9-59-103-109-25.ngrok-free.app",
+  //     "/customer/auth/register": "https://78b9-59-103-109-25.ngrok-free.app",
+  //     "/customer/auth/login": "https://78b9-59-103-109-25.ngrok-free.app",
+  //     "/customer/updatecustomer": "https://78b9-59-103-109-25.ngrok-free.app",
 
 
-    },
-    // https: true,
-  },
+  //   },
+  //   // https: true,
+  // },
 });
