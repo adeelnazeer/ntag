01d910f8673863527007b62c4222e83907f81a1c
adeel <adeelcomsats070@gmail.com>
2025-11-30 14:29:55 +0500

    Fixed : OR language issue

--- android/app/src/main/java/ntag/portal/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package ntag.portal;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- android/src/utilities/validateEthiopianPhone.js
+++
@@ -1,9 +0,0 @@
-// utils/validations.js
-export const validateEthiopianPhone = (value) => {
-    if (!value) return "Phone number is required";
-    const phoneNumber = value.replace("+251", "");
-    if (!phoneNumber.startsWith("9")) {
-      return "Ethiopian phone numbers must start with 9";
-    }
-    return true;
-  };
